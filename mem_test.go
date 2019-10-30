1445ec0c284efc688555a49368bf7734c7882792
mosalut <ludaoyuan@mosalut.com>
2019-10-30 19:48:26 +0800

    从项目中剥离出来作为工具，代码其实很简单
    On branch mosalut
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	new file:   .gitignore
    	modified:   README.md
    	new file:   mem_test.go
    	new file:   type.go

---
+++ mem_test.go
@@ -0,0 +1,46 @@
+package mem
+
+import (
+	"testing"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func insert() error {
+	db, err := sql.Open("mysql", "mosalut:12345678@tcp(172.19.70.171)/jx?charset=utf8")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("insert into account values(null, ?, md5(?), null, ?, null, ?, null, 0, 0, null, null)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec("account", "password", "email@email.com", "nick")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestMem(t *testing.T) {
+	err := insert()
+	if err != nil {
+		dbError := &dbErr{}
+		err := dbError.mapping(err)
+		if err != nil {
+			t.Error(err.Error())
+			return
+		}
+
+		t.Log(dbError.Number)
+		t.Log(dbError.Message)
+		t.Log(dbError.Error())
+		return
+	}
+}
